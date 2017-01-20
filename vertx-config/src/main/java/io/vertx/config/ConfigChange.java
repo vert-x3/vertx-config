7600866187ab738147137a878ea92d6f38f23e7f
Clement Escoffier <clement.escoffier@gmail.com>
2017-01-20 09:40:00 +0100

    Add javadoc
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17554109

--- vertx-config/src/main/java/io/vertx/config/ConfigChange.java
+++ vertx-config/src/main/java/io/vertx/config/ConfigChange.java
@@ -3,6 +3,8 @@ package io.vertx.config;
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.core.json.JsonObject;
 
+import java.util.Objects;
+
 /**
  * A structure representing a configuration change.
  *
@@ -15,15 +17,30 @@ public class ConfigChange {
 
   private JsonObject newConfiguration;
 
+  /**
+   * Creates a new {@link ConfigChange} instance.
+   *
+   * @param prevConf the previous configuration, may be {@code null}. In this case, an empty JSON object is used.
+   * @param newConf  the new configuration, may be {@code null}. In this case, an empty JSON object is used.
+   */
   public ConfigChange(JsonObject prevConf, JsonObject newConf) {
     setPreviousConfiguration(prevConf);
     setNewConfiguration(newConf);
   }
 
+  /**
+   * @return the previous configuration, never {@code null}, but potentially empty
+   */
   public JsonObject getPreviousConfiguration() {
     return previousConfiguration;
   }
 
+  /**
+   * Sets the previous configuration.
+   *
+   * @param conf the configuration, may be {@code null}. In this case an empty JSON object is used.
+   * @return the current instance of {@link ConfigChange}
+   */
   public ConfigChange setPreviousConfiguration(JsonObject conf) {
     if (conf == null) {
       this.previousConfiguration = new JsonObject();
@@ -33,10 +50,19 @@ public class ConfigChange {
     return this;
   }
 
+  /**
+   * @return the new configuration, never {@code null}, but potentially empty.
+   */
   public JsonObject getNewConfiguration() {
     return newConfiguration;
   }
 
+  /**
+   * Sets the new configuration.
+   *
+   * @param conf the new configuration, may be {@code null}. In this case, an empty JSON object is used.
+   * @return the current instance of {@link ConfigChange}
+   */
   public ConfigChange setNewConfiguration(JsonObject conf) {
     if (conf == null) {
       this.newConfiguration = new JsonObject();
@@ -46,21 +72,38 @@ public class ConfigChange {
     return this;
   }
 
+  /**
+   * Creates a new instance from {@link ConfigChange} using empty JSON Object for both the old and new configuration.
+   */
   public ConfigChange() {
     newConfiguration = new JsonObject();
     previousConfiguration = new JsonObject();
   }
 
+  /**
+   * Creates a new instance of {@link ConfigChange} copying the values stored in the given object.
+   *
+   * @param other the instance to copy
+   */
   public ConfigChange(ConfigChange other) {
-    this.previousConfiguration = other.previousConfiguration;
-    this.newConfiguration = other.newConfiguration;
+    this.previousConfiguration = other.previousConfiguration.copy();
+    this.newConfiguration = other.newConfiguration.copy();
   }
 
+  /**
+   * Creates a new {@link ConfigChange} instance from the given JSON object.
+   *
+   * @param json the json object, must not be {@code null}
+   */
   public ConfigChange(JsonObject json) {
+    Objects.requireNonNull(json);
     this.setNewConfiguration(json.getJsonObject("newConfiguration", new JsonObject()));
     this.setPreviousConfiguration(json.getJsonObject("previousConfiguration", new JsonObject()));
   }
 
+  /**
+   * @return the JSON representation of the current {@link ConfigChange} instance.
+   */
   public JsonObject toJson() {
     JsonObject json = new JsonObject();
     json.put("newConfiguration", newConfiguration);
