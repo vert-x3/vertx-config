555529f5e2959f3ab16c779d932725e0ff3863dc
Clement Escoffier <clement.escoffier@gmail.com>
2017-01-20 09:44:36 +0100

    Add javadoc
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17554258

--- vertx-config/src/main/java/io/vertx/config/spi/ConfigProcessor.java
+++ vertx-config/src/main/java/io/vertx/config/spi/ConfigProcessor.java
@@ -7,12 +7,31 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 
 /**
+ * A processor transforms a chunk of configuration retrieved from a configuration store as a {@link Buffer} to a
+ * {@link JsonObject}.
+ *
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
  */
 public interface ConfigProcessor {
 
+  /**
+   * Name of the processor, generally the name of the format it handles.
+   *
+   * @return the name
+   */
   String name();
 
+  /**
+   * Transforms the given {@code input} into a {@link JsonObject}. This is an asynchronous non-blocking
+   * transformation. The result is passed to the given {@code Handler}. If the transformation fails, the passed
+   * {@link AsyncResult} would be marked as failed. On success, the result contains the {@link JsonObject}.
+   *
+   * @param vertx         the Vert.x instance
+   * @param configuration the processor configuration, may be {@code null}
+   * @param input         the input, must not be {@code null}
+   * @param handler       the result handler, must not be {@code null}. The handler will be called in the same context as
+   *                      the caller.
+   */
   void process(Vertx vertx, JsonObject configuration, Buffer input, Handler<AsyncResult<JsonObject>> handler);
 
 }
