61fea91f8be6810cbda89403e8d61abfa6d8e4db
Clement Escoffier <clement.escoffier@gmail.com>
2017-01-20 09:29:27 +0100

    Add javadoc
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17553425
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17553455
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17553488
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17553560
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17553600
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17553666
    https://github.com/vert-x3/vertx-configuration/pull/1#pullrequestreview-17553705

--- vertx-config/src/main/java/io/vertx/config/ConfigStoreOptions.java
+++ vertx-config/src/main/java/io/vertx/config/ConfigStoreOptions.java
@@ -6,6 +6,11 @@ import io.vertx.core.json.JsonObject;
 import java.util.Objects;
 
 /**
+ * Data object representing the configuration of a configuration store. This object describes the configuration of a
+ * chunk of configuration that you retrieve. It specifies its type (type of configuration store), the format of the
+ * retrieved configuration chunk, and you can also configures the store if it needs configuration to
+ * retrieve the configuration chunk.
+ *
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
  */
 @DataObject
@@ -46,29 +51,56 @@ public class ConfigStoreOptions {
     return json;
   }
 
+  /**
+   * @return the configuration type, it specified from which store the configuration is coming.
+   */
   public String getType() {
     return type;
   }
 
+  /**
+   * Sets the configuration type
+   *
+   * @param type the type
+   * @return the current instance of {@link ConfigStoreOptions}
+   */
   public ConfigStoreOptions setType(String type) {
     this.type = type;
     return this;
   }
 
+  /**
+   * @return the configuration of the store, may be {@code null} if not needed.
+   */
   public JsonObject getConfig() {
     return config;
   }
 
+  /**
+   * Sets the configuration of the store
+   *
+   * @param config the data, can be {@code null}
+   * @return the current instance of {@link ConfigStoreOptions}
+   */
   public ConfigStoreOptions setConfig(JsonObject config) {
     this.config = config;
     return this;
   }
 
 
+  /**
+   * @return the format of the configuration that is retrieved from the store.
+   */
   public String getFormat() {
     return format;
   }
 
+  /**
+   * Sets the format of the configuration that is retrieved from the store.
+   *
+   * @param format the format, must not be {@code null}.
+   * @return the current instance of {@link ConfigStoreOptions}
+   */
   public ConfigStoreOptions setFormat(String format) {
     Objects.requireNonNull(format);
     this.format = format;
