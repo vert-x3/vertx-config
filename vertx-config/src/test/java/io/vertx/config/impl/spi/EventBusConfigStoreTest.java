aeb70243fcf45a43a57f2366657670909832f8ab
Thomas Segismont <tsegismo@redhat.com>
2018-08-01 11:01:22 +0200

    Fixed intermittent StackOverflowError in EventBusConfigStoreTest
    testWithPublish might fail with SOE because it retries on the same thread if config has not been dispatched yet.

--- vertx-config/src/test/java/io/vertx/config/impl/spi/EventBusConfigStoreTest.java
+++ vertx-config/src/test/java/io/vertx/config/impl/spi/EventBusConfigStoreTest.java
@@ -17,8 +17,6 @@
 
 package io.vertx.config.impl.spi;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
@@ -26,12 +24,7 @@ import io.vertx.ext.unit.TestContext;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNull.notNullValue;
 
 /**
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
@@ -45,87 +38,48 @@ public class EventBusConfigStoreTest extends ConfigStoreTestBase {
 
   @Test
   public void testWithSend(TestContext tc) {
-    Async async = tc.async();
-    store = factory.create(vertx, new JsonObject()
-      .put("address", "config")
-    );
-
-    getJsonConfiguration(vertx, store, ar -> {
-      assertThat(ar.result().isEmpty()).isTrue();
-
-      vertx.eventBus().send("config", new JsonObject(HttpConfigStoreTest.JSON));
-
-      AtomicReference<JsonObject> received = new AtomicReference<>();
-      while (received.get() == null) {
-        getJsonConfiguration(vertx, store, ar2 -> {
-          if (!ar2.result().isEmpty()) {
-            received.set(ar2.result());
-          }
-        });
-      }
-      await().untilAtomic(received, is(notNullValue()));
-
-      ConfigChecker.check(received.get());
-      async.complete();
-    });
+    testEventBusConfigStore(tc, new JsonObject(HttpConfigStoreTest.JSON), true);
   }
 
   @Test
   public void testWithSendWithBuffer(TestContext tc) {
-    Async async = tc.async();
-    store = factory.create(vertx, new JsonObject()
-      .put("address", "config")
-    );
-
-    getJsonConfiguration(vertx, store, ar -> {
-      assertThat(ar.result().isEmpty()).isTrue();
-
-      vertx.eventBus().send("config", Buffer.buffer(HttpConfigStoreTest.JSON));
-
-      AtomicReference<JsonObject> received = new AtomicReference<>();
-      while (received.get() == null) {
-        getJsonConfiguration(vertx, store, ar2 -> {
-          if (!ar2.result().isEmpty()) {
-            received.set(ar2.result());
-          }
-        });
-      }
-      await().untilAtomic(received, is(notNullValue()));
-
-      ConfigChecker.check(received.get());
-      async.complete();
-    });
+    testEventBusConfigStore(tc, Buffer.buffer(HttpConfigStoreTest.JSON), true);
   }
-
-  private Handler<AsyncResult<JsonObject>> handler;
 
   @Test
   public void testWithPublish(TestContext tc) {
-    Async async = tc.async();
-    store = factory.create(vertx, new JsonObject()
-      .put("address", "config")
-    );
+    testEventBusConfigStore(tc, new JsonObject(HttpConfigStoreTest.JSON), false);
+  }
 
+  @Test
+  public void testWithPublishWithBuffer(TestContext tc) {
+    testEventBusConfigStore(tc, Buffer.buffer(HttpConfigStoreTest.JSON), false);
+  }
+
+  private void testEventBusConfigStore(TestContext tc, Object config, boolean send) {
+    store = factory.create(vertx, new JsonObject().put("address", "config"));
+    Async async = tc.async(2);
     getJsonConfiguration(vertx, store, ar -> {
-      if (ar.failed()) {
-        ar.cause().printStackTrace();
-      }
       assertThat(ar.result().isEmpty()).isTrue();
-
-      vertx.eventBus().publish("config", new JsonObject(HttpConfigStoreTest.JSON));
-
-      handler = (ar2) -> {
-        if (ar2.result().isEmpty()) {
-          // Retry as the publication may not have been dispatched yet.
-          getJsonConfiguration(vertx, store, handler);
-          return;
-        }
-        ConfigChecker.check(ar2.result());
-        async.complete();
-      };
-
-      getJsonConfiguration(vertx, store, handler);
+      async.countDown();
+      if (send) {
+        vertx.eventBus().send("config", config);
+      } else {
+        vertx.eventBus().publish("config", config);
+      }
+      vertx.setTimer(10, tid -> getConfigAndCheck(tc, async));
     });
   }
 
+  private void getConfigAndCheck(TestContext tc, Async async) {
+    getJsonConfiguration(vertx, store, tc.asyncAssertSuccess(res -> {
+      if (res.isEmpty()) {
+        // Retry as the publication may not have been dispatched yet.
+        vertx.setTimer(10, tid -> getConfigAndCheck(tc, async));
+      } else {
+        ConfigChecker.check(res);
+        async.countDown();
+      }
+    }));
+  }
 }
