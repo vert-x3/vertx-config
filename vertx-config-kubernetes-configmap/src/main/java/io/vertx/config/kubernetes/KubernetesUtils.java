4b3675473a0d383ea93610e878066bc1506a19bc
Michael Panchenko <panchmp@gmail.com>
2020-03-30 18:23:43 -0400

    Use try-with-resources statement ensures that resource is close
    Signed-off-by: Michael Panchenko <panchmp@gmail.com>

--- vertx-config-kubernetes-configmap/src/main/java/io/vertx/config/kubernetes/KubernetesUtils.java
+++ vertx-config-kubernetes-configmap/src/main/java/io/vertx/config/kubernetes/KubernetesUtils.java
@@ -17,16 +17,12 @@
 
 package io.vertx.config.kubernetes;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 /**
  * Utility methods for Kubernetes.
  *
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
  */
 public class KubernetesUtils {
-
 
   public static final String OPENSHIFT_KUBERNETES_TOKEN_FILE = "/var/run/secrets/kubernetes.io/serviceaccount/token";
 
