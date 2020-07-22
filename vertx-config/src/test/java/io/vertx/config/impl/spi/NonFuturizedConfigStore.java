99a9b8552a94f133bb86cd52230dad12a6c83631
Thomas Segismont <tsegismo@redhat.com>
2020-07-22 10:31:04 +0200

    Add legacy config store test

---
+++ vertx-config/src/test/java/io/vertx/config/impl/spi/NonFuturizedConfigStore.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.vertx.config.impl.spi;
+
+import io.vertx.config.spi.ConfigStore;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
+
+public class NonFuturizedConfigStore implements ConfigStore {
+
+  private final Vertx vertx;
+  private final JsonObject configuration;
+
+  public NonFuturizedConfigStore(Vertx vertx, JsonObject configuration) {
+    this.vertx = vertx;
+    this.configuration = configuration;
+  }
+
+  @Override
+  public void get(Handler<AsyncResult<Buffer>> completionHandler) {
+    vertx.executeBlocking(promise -> {
+      promise.complete(configuration.getBuffer("foo"));
+    }, completionHandler);
+  }
+}
