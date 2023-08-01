f650b9c19601883805df624b627fc99066ecb6b4
Julien Viet <julien@julienviet.com>
2023-08-01 13:01:17 +0200

    Copy FileSelector match from launcher

--- vertx-config/src/main/java/io/vertx/config/spi/utils/FileSet.java
+++ vertx-config/src/main/java/io/vertx/config/spi/utils/FileSet.java
@@ -19,7 +19,6 @@ package io.vertx.config.spi.utils;
 
 import io.vertx.config.spi.ConfigProcessor;
 import io.vertx.core.*;
-import io.vertx.core.impl.launcher.commands.FileSelector;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
@@ -71,7 +70,161 @@ public class FileSet {
   }
 
   private boolean matches(String path) {
-    return FileSelector.match(pattern, path, false);
+    return match(pattern, path, false);
+  }
+
+  /**
+   * Tests whether or not a string matches against a pattern.
+   * The pattern may contain two special characters:<br>
+   * '*' means zero or more characters<br>
+   * '?' means one and only one character
+   *
+   * @param pattern         The pattern to match against.
+   *                        Must not be{@code null}.
+   * @param str             The string which must be matched against the pattern.
+   *                        Must not be{@code null}.
+   * @param isCaseSensitive Whether or not matching should be performed
+   *                        case sensitively.
+   * @return {@code true} if the string matches against the pattern,
+   * or {@code false} otherwise.
+   */
+  public static boolean match(String pattern, String str, boolean isCaseSensitive) {
+    char[] patArr = pattern.toCharArray();
+    char[] strArr = str.toCharArray();
+    return match(patArr, strArr, isCaseSensitive);
+  }
+
+  private static boolean match(char[] patArr, char[] strArr, boolean isCaseSensitive) {
+    int patIdxStart = 0;
+    int patIdxEnd = patArr.length - 1;
+    int strIdxStart = 0;
+    int strIdxEnd = strArr.length - 1;
+    char ch;
+
+    boolean containsStar = false;
+    for (char aPatArr : patArr) {
+      if (aPatArr == '*') {
+        containsStar = true;
+        break;
+      }
+    }
+
+    if (!containsStar) {
+      // No '*'s, so we make a shortcut
+      if (patIdxEnd != strIdxEnd) {
+        return false; // Pattern and string do not have the same size
+      }
+      for (int i = 0; i <= patIdxEnd; i++) {
+        ch = patArr[i];
+        if (ch != '?' && !equals(ch, strArr[i], isCaseSensitive)) {
+          return false; // Character mismatch
+        }
+      }
+      return true; // String matches against pattern
+    }
+
+    if (patIdxEnd == 0) {
+      return true; // Pattern contains only '*', which matches anything
+    }
+
+    // Process characters before first star
+    while ((ch = patArr[patIdxStart]) != '*' && strIdxStart <= strIdxEnd) {
+      if (ch != '?' && !equals(ch, strArr[strIdxStart], isCaseSensitive)) {
+        return false; // Character mismatch
+      }
+      patIdxStart++;
+      strIdxStart++;
+    }
+    if (strIdxStart > strIdxEnd) {
+      return checkOnlyStartsLeft(patArr, patIdxStart, patIdxEnd);
+    }
+
+    // Process characters after last star
+    while ((ch = patArr[patIdxEnd]) != '*' && strIdxStart <= strIdxEnd) {
+      if (ch != '?' && !equals(ch, strArr[strIdxEnd], isCaseSensitive)) {
+        return false; // Character mismatch
+      }
+      patIdxEnd--;
+      strIdxEnd--;
+    }
+    if (strIdxStart > strIdxEnd) {
+      // All characters in the string are used. Check if only '*'s are
+      // left in the pattern. If so, we succeeded. Otherwise failure.
+      return checkOnlyStartsLeft(patArr, patIdxStart, patIdxEnd);
+    }
+
+    // process pattern between stars. padIdxStart and patIdxEnd point
+    // always to a '*'.
+    while (patIdxStart != patIdxEnd && strIdxStart <= strIdxEnd) {
+      int patIdxTmp = -1;
+      for (int i = patIdxStart + 1; i <= patIdxEnd; i++) {
+        if (patArr[i] == '*') {
+          patIdxTmp = i;
+          break;
+        }
+      }
+      if (patIdxTmp == patIdxStart + 1) {
+        // Two stars next to each other, skip the first one.
+        patIdxStart++;
+        continue;
+      }
+      // Find the pattern between padIdxStart & padIdxTmp in str between
+      // strIdxStart & strIdxEnd
+      int patLength = (patIdxTmp - patIdxStart - 1);
+      int strLength = (strIdxEnd - strIdxStart + 1);
+      int foundIdx = -1;
+      strLoop:
+      for (int i = 0; i <= strLength - patLength; i++) {
+        for (int j = 0; j < patLength; j++) {
+          ch = patArr[patIdxStart + j + 1];
+          if (ch != '?' && !equals(ch, strArr[strIdxStart + i + j], isCaseSensitive)) {
+            continue strLoop;
+          }
+        }
+
+        foundIdx = strIdxStart + i;
+        break;
+      }
+
+      if (foundIdx == -1) {
+        return false;
+      }
+
+      patIdxStart = patIdxTmp;
+      strIdxStart = foundIdx + patLength;
+    }
+
+    // All characters in the string are used. Check if only '*'s are left
+    // in the pattern. If so, we succeeded. Otherwise failure.
+    return checkOnlyStartsLeft(patArr, patIdxStart, patIdxEnd);
+  }
+
+  private static boolean checkOnlyStartsLeft(char[] patArr, int patIdxStart, int patIdxEnd) {
+    // All characters in the string are used. Check if only '*'s are
+    // left in the pattern. If so, we succeeded. Otherwise failure.
+    for (int i = patIdxStart; i <= patIdxEnd; i++) {
+      if (patArr[i] != '*') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Tests whether two characters are equal.
+   */
+  private static boolean equals(char c1, char c2, boolean isCaseSensitive) {
+    if (c1 == c2) {
+      return true;
+    }
+    if (!isCaseSensitive) {
+      // NOTE: Try both upper case and lower case as done by String.equalsIgnoreCase()
+      if (Character.toUpperCase(c1) == Character.toUpperCase(c2)
+        || Character.toLowerCase(c1) == Character.toLowerCase(c2)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /**
