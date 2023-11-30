09dd58901f3a788858dce683018ece3d02bded9c
Julien Viet <julien@julienviet.com>
2023-11-30 10:22:22 +0100

    Use the new @JsonGen annotation

--- vertx-config/src/main/java/io/vertx/config/ConfigRetrieverOptions.java
+++ vertx-config/src/main/java/io/vertx/config/ConfigRetrieverOptions.java
@@ -18,6 +18,7 @@
 package io.vertx.config;
 
 import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.annotations.JsonGen;
 import io.vertx.core.json.JsonObject;
 
 import java.util.ArrayList;
@@ -28,7 +29,8 @@ import java.util.List;
  *
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
  */
-@DataObject(generateConverter = true)
+@DataObject
+@JsonGen(publicConverter = false)
 public class ConfigRetrieverOptions {
 
   /**
