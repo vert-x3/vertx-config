f6ec82b4026f0016894277719a826cd6b5ccc143
Julien Viet <julien@julienviet.com>
2024-03-27 17:16:46 +0100

    Fix incorrect contextual error report

--- vertx-config/src/main/java/io/vertx/config/impl/ConfigRetrieverImpl.java
+++ vertx-config/src/main/java/io/vertx/config/impl/ConfigRetrieverImpl.java
@@ -263,12 +263,12 @@ public class ConfigRetrieverImpl implements ConfigRetriever {
       .map(s -> s.get(context.owner()))
       .collect(Collectors.toList());
 
-    return CompositeFuture.all(futures).map(compositeFuture -> {
+    return context.succeededFuture().compose(v -> CompositeFuture.all(futures).map(compositeFuture -> {
       // Merge the different futures
       JsonObject json = new JsonObject();
       futures.forEach(future -> json.mergeIn((JsonObject) future.result(), true));
       return json;
-    }).map(json -> processor != null ? processor.apply(json) : json);
+    }).map(json -> processor != null ? processor.apply(json) : json));
   }
 
   /**
