804687148bb48479b4cb33c14d4fec2b62784150
Thomas Segismont <tsegismont@gmail.com>
2025-01-07 10:30:14 +0100

    Fix HttpGreetingVerticle example
    Use the same code as in the main branch (except the verticle in the main branch extends VerticleBase).
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-config/src/main/java/examples/HttpGreetingVerticle.java
+++ vertx-config/src/main/java/examples/HttpGreetingVerticle.java
@@ -2,19 +2,20 @@ package examples;
 
 import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.json.JsonObject;
+import io.vertx.core.Promise;
 
 public class HttpGreetingVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
+  public void start(Promise<Void> startPromise) {
     ConfigRetriever retriever = ConfigRetriever.create(vertx);
-    retriever.getConfig().onComplete(result -> {
-      JsonObject json = result.result();
-
-      vertx.createHttpServer()
+    retriever
+      .getConfig()
+      .compose(json -> vertx.createHttpServer()
         .requestHandler(req -> req.response().end(json.getString("message")))
-        .listen(json.getInteger("port"));
-    });
+        .listen(json.getInteger("port"))
+      )
+      .<Void>mapEmpty()
+      .onComplete(startPromise);
   }
 }
